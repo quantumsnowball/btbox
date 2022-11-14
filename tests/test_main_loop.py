adba3a8d0e95b4e10514b2aacdcb25f704e812b6
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-15 07:53:09 +0800

    test: on dummy and real data

--- tests/test_main_loop.py
+++ tests/test_main_loop.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import numpy as np
 import btbox
 import btbox.backtest
 from btbox.broker import Broker
@@ -10,12 +11,29 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-dataframe = pd.read_csv('tests/SPY.csv', index_col='Date', parse_dates=True)
+def test_main_loop_on_dummy_data():
+    dates = pd.date_range(start='2010-01-01', end='2020-12-31')
+    dataframe = pd.DataFrame(np.random.randint(
+        0, 1000, size=len(dates)), index=dates)
+
+    class CustomStrategy(btbox.Strategy):
+        name = 'test main loop on dummy data'
+
+        def step(self, i: int, now: datetime, broker: Broker):
+            # this contains the algo logics
+            if i % 1000 == 0:
+                logger.info(dict(i=i, now=now, broker=broker))
+                assert now == broker._now == broker._market._now
+
+    btbox.create_backtest(CustomStrategy, dataframe).run()
 
 
 def test_main_loop():
+    dataframe = pd.read_csv('tests/SPY.csv',
+                            index_col='Date', parse_dates=True)
+
     class CustomStrategy(btbox.Strategy):
-        name = 'Algo'
+        name = 'test main loop'
 
         def step(self, i: int, now: datetime, broker: Broker):
             # this contains the algo logics
