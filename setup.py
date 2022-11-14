e43963766436c64f7fe592fc73db7695e0e7dae3
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-14 10:08:10 +0800

    package: setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='btbox',
+    version='1.0',
+    description='btbox - a box for backtesting',
+    url='https://github.com/quantumsnowball/btbox',
+    author='Quantum Snowball',
+    author_email='quantum.snowball@gmail.com',
+    license='MIT',
+    packages=['btbox'],
+    zip_safe=False
+)
