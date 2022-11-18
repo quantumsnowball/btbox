67d508235fcea43f1152dda96f484723808efb8c
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-18 09:17:48 +0800

    proj: export all main classes under root package

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 BTBox is a simple to use event-driven backtesting tool written in Python. The goal of this project is to provide a simple interface for backtesting trading strategy.
 
+# Install
+
+```
+pip install -e .
+```
+
 # Design
 
 Chainn of object compositions as follows:
--- btbox/__init__.py
+++ btbox/__init__.py
@@ -1,3 +1,11 @@
+from btbox.backtest import Backtest
+from btbox.broker import Broker
+from btbox.datasource import DataSource
+from btbox.job import Job
+from btbox.market import Market
 from btbox.strategy import Strategy
+
 from btbox.job.utils import create_job
 from btbox.backtest.utils import create_backtest
+
+from btbox.datasource.utils import import_yahoo_csv
