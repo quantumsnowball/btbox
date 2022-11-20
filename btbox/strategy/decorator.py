9b980bbf0bca11c2592dbdc8bfed00d1d74ff393
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-21 07:45:42 +0800

    refactor: decorator @wrap(func)

--- btbox/strategy/decorator.py
+++ btbox/strategy/decorator.py
@@ -1,12 +1,15 @@
+from functools import wraps
 from btbox.broker import Broker
 
 
 def interval(n: int, *, initial: bool = True):
     def decorator(fn_step):
+        @wraps(fn_step)
         def wrpfn_step(self, i: int, b: Broker):
             if i % n == 0:
                 return fn_step(self, b)
 
+        @wraps(fn_step)
         def wrpfn_step_skip_initial(self, i: int, b: Broker):
             if i % n == 0 and i > 0:
                 return fn_step(self, b)
