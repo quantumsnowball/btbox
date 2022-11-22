6ab717f8f8dc4f7a9253bbfde4ead27f9c78fb18
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-22 16:16:47 +0800

    refactor: tidy up journals test cases

--- tests/backtest/results/test_journal.py
+++ tests/backtest/results/test_journal.py
@@ -5,41 +5,41 @@ from btbox.backtest.utils import create_backtest
 from btbox.strategy.decorator import interval
 
 
+dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
+
+
+class ST(Strategy):
+    def step(self, i: int, b: Broker):
+        # continuous line
+        self.journal.mark(8888, 'line-8888')
+        self.journal.mark(9999, 'line-9999')
+        # individual signal
+        if i % 2 == 0 and i > 0:
+            self.journal.mark(2, 'every-2d')
+        if i % 4 == 0 and i > 0:
+            self.journal.mark(4, 'every-4d')
+        if i % 5 == 0 and i > 0:
+            self.journal.mark(4, 'every-5d')
+        # sparse points
+        if i % 1000 == 0:
+            self.journal.mark(1111, 'point-1111')
+            self.journal.mark(2222, 'point-2222')
+
+
 def test_bfill_ffill():
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Line'
-
-        @interval(1000)
-        def step(self, b: Broker):
-            self.journal.mark(8888, 'line-8888')
-            self.journal.mark(9999, 'line-9999')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    df_pre = results['Line'].journals['line-8888'].values
-    df_bfill = results['Line'].journals['line-8888'].bfill.values
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
+    df_pre = result.journals['point-1111'].values
+    df_bfill = result.journals['point-1111'].bfill.values
     assert df_bfill.equals(df_pre.bfill())
-    df_pre = results['Line'].journals['line-9999'].values
-    df_ffill = results['Line'].journals['line-9999'].ffill.values
+    df_pre = result.journals['point-2222'].values
+    df_ffill = result.journals['point-2222'].ffill.values
     assert df_ffill.equals(df_pre.ffill())
 
 
 def test_plot_line(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Line'
-
-        @interval(1)
-        def step(self, b: Broker):
-            self.journal.mark(8888, 'line-8888')
-            self.journal.mark(9999, 'line-9999')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Line']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_Figure = mocker.patch(
         'btbox.backtest.results.selected.utils.Figure')
     fn_Scatter = mocker.patch(
@@ -50,47 +50,22 @@ def test_plot_line(mocker):
 
 
 def test_plot_scatter(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Scatter'
-
-        def step(self, i: int, b: Broker):
-            if i % 2 == 0 and i > 0:
-                self.journal.mark(2, 'Every 2 Days')
-            if i % 4 == 0 and i > 0:
-                self.journal.mark(4, 'Every 4 Days')
-            if i % 5 == 0 and i > 0:
-                self.journal.mark(4, 'Every 5 Days')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Scatter']
-    assert result.journals['Every 2 Days', 'Every 5 Days'].values.shape[1] == 2
-    assert result.journals['Every 2 Days', 'Every 4 Days'].values.shape[1] == 2
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
+    assert result.journals['every-2d', 'every-5d'].values.shape[1] == 2
+    assert result.journals['every-2d', 'every-4d'].values.shape[1] == 2
     fn_Figure = mocker.patch(
         'btbox.backtest.results.selected.utils.Figure')
     fn_Scatter = mocker.patch(
         'btbox.backtest.results.selected.utils.Scatter')
-    result.journals['Every 2 Days', 'Every 5 Days'].plot_scatter()
+    result.journals['every-2d', 'every-5d'].plot_scatter()
     fn_Figure.assert_called_once()
     fn_Scatter.assert_called()
 
 
 def test_plot_line_on_price(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Line'
-
-        @interval(1)
-        def step(self, b: Broker):
-            self.journal.mark(8888, 'line-8888')
-            self.journal.mark(9999, 'line-9999')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Line']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_Figure = mocker.patch(
         'btbox.backtest.results.selected.utils.Figure')
     fn_Scatter = mocker.patch(
@@ -101,72 +76,32 @@ def test_plot_line_on_price(mocker):
 
 
 def test_plot_scatter_on_nav(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Nav-Scatter'
-
-        def step(self, i: int, b: Broker):
-            if i % 2 == 0 and i > 0:
-                self.journal.mark(2, 'Every 2 Days')
-            if i % 4 == 0 and i > 0:
-                self.journal.mark(4, 'Every 4 Days')
-            if i % 5 == 0 and i > 0:
-                self.journal.mark(4, 'Every 5 Days')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Nav-Scatter']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_Figure = mocker.patch(
         'btbox.backtest.results.selected.utils.Figure')
     fn_Scatter = mocker.patch(
         'btbox.backtest.results.selected.utils.Scatter')
-    result.journals['Every 2 Days', 'Every 5 Days'].plot_scatter_on_nav()
+    result.journals['every-2d', 'every-5d'].plot_scatter_on_nav()
     fn_Figure.assert_called_once()
     fn_Scatter.assert_called()
 
 
 def test_plot_scatter_on_price(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Nav-Scatter'
-
-        def step(self, i: int, b: Broker):
-            if i % 2 == 0 and i > 0:
-                self.journal.mark(2, 'Every 2 Days')
-            if i % 4 == 0 and i > 0:
-                self.journal.mark(4, 'Every 4 Days')
-            if i % 5 == 0 and i > 0:
-                self.journal.mark(4, 'Every 5 Days')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Nav-Scatter']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_Figure = mocker.patch(
         'btbox.backtest.results.selected.utils.Figure')
     fn_Scatter = mocker.patch(
         'btbox.backtest.results.selected.utils.Scatter')
-    result.journals['Every 2 Days',
-                    'Every 5 Days'].plot_scatter_on_price('SPY')
+    result.journals['every-2d', 'every-5d'].plot_scatter_on_price('SPY')
     fn_Figure.assert_called_once()
     fn_Scatter.assert_called()
 
 
 def test_plot_line_under_nav(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Nav-Line'
-
-        @interval(1)
-        def step(self, b: Broker):
-            self.journal.mark(8888, 'line-8888')
-            self.journal.mark(9999, 'line-9999')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Nav-Line']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_make_subplots = mocker.patch(
         'btbox.backtest.results.selected.utils.make_subplots')
     fn_Scatter = mocker.patch(
@@ -177,19 +112,8 @@ def test_plot_line_under_nav(mocker):
 
 
 def test_plot_line_under_price(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Price-Line'
-
-        @interval(1)
-        def step(self, b: Broker):
-            self.journal.mark(8888, 'line-8888')
-            self.journal.mark(9999, 'line-9999')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Price-Line']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_make_subplots = mocker.patch(
         'btbox.backtest.results.selected.utils.make_subplots')
     fn_Scatter = mocker.patch(
@@ -200,26 +124,12 @@ def test_plot_line_under_price(mocker):
 
 
 def test_plot_scatter_under_nav(mocker):
-    dfs = {'SPY': import_yahoo_csv('tests/_data_/SPY_bar1day.csv')}
-
-    class S1(Strategy):
-        name = 'Nav-Scatter'
-
-        def step(self, i: int, b: Broker):
-            if i % 2 == 0 and i > 0:
-                self.journal.mark(2, 'Every 2 Days')
-            if i % 4 == 0 and i > 0:
-                self.journal.mark(4, 'Every 4 Days')
-            if i % 5 == 0 and i > 0:
-                self.journal.mark(4, 'Every 5 Days')
-
-    results = create_backtest(
-        [S1, ], dfs, start='2020-01-01', window=30).run()
-    result = results['Nav-Scatter']
+    result = create_backtest([ST, ], dfs,
+                             start='2020-01-01', window=30).run()['ST']
     fn_make_subplots = mocker.patch(
         'btbox.backtest.results.selected.utils.make_subplots')
     fn_Scatter = mocker.patch(
         'btbox.backtest.results.selected.utils.Scatter')
-    result.journals['Every 2 Days', 'Every 5 Days'].plot_scatter_under_nav()
+    result.journals['every-2d', 'every-5d'].plot_scatter_under_nav()
     fn_make_subplots.assert_called_once()
     fn_Scatter.assert_called()
