3263261519b0a2d611ea16cd04bd3972839c84f0
Quantum Snowball <quantum.snowball@gmail.com>
2022-11-23 10:47:26 +0800

    feat: default log_y for selected.nav.plot

--- btbox/backtest/results/selected/nav.py
+++ btbox/backtest/results/selected/nav.py
@@ -11,8 +11,10 @@ class Nav:
         self._result = result
         self._nav = self._result.report.nav
 
-    def plot(self, **kwargs_line: Any) -> None:
+    def plot(self,
+             log_y: bool = True,
+             **kwargs_line: Any) -> None:
         if 'title' not in kwargs_line:
             kwargs_line['title'] = self._name
-        fig = px.line(self._nav, **kwargs_line)
+        fig = px.line(self._nav, log_y=log_y, **kwargs_line)
         fig.show()
